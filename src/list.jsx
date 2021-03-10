57efd15b2307e9b24e4438a5783981779f1c1d6b
Sujay S N <80213368+SujaySN46@users.noreply.github.com>
2021-03-10 13:52:18 +0530

    Initial commit

---
+++ src/list.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>rice</li>
+        <li>pizza</li>
+        <li>juice</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
